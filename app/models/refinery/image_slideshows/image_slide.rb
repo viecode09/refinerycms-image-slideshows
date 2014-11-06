9c469af6ef0a8866eff0f699650c90a148ca8e76
Brice Sanchez <brice@bisscomm.com>
2014-11-06 16:25:34 -0500

    Remove attr_accessible

--- app/models/refinery/image_slideshows/image_slide.rb
+++ app/models/refinery/image_slideshows/image_slide.rb
@@ -3,15 +3,13 @@ module Refinery
     class ImageSlide < Refinery::Core::BaseModel
       self.table_name = 'refinery_image_slides'
 
-      attr_accessible :title, :image_id, :caption, :link_url, :position, :image_slideshow_id
-
       acts_as_indexed :fields => [:title]
 
       validates :title, :presence => true
       validates :image_id, :presence => true
 
       belongs_to :image_slideshow
-      belongs_to :image
+      belongs_to :image, :class_name => '::Refinery::Image'
 
       delegate :height, :width, :to => :image_slideshow
     end
--- app/models/refinery/image_slideshows/image_slideshow.rb
+++ app/models/refinery/image_slideshows/image_slideshow.rb
@@ -3,8 +3,6 @@ module Refinery
     class ImageSlideshow < Refinery::Core::BaseModel
       self.table_name = 'refinery_image_slideshows'
 
-      attr_accessible :title, :position, :js_config, :height, :width
-
       acts_as_indexed :fields => [:title]
 
       validates :title, :presence => true, :uniqueness => true
